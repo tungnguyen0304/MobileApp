3570e1e7fda7b5b9ba9b9ffd46774b790d0cc4ce
duchuykg <duchuydeptrainhatthegioi@gmail.com>
2023-04-03 10:53:09 +0700

    import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { RunsModule } from './runs/runs.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule, RunsModule],
   controllers: [AppController],
   providers: [AppService],
 })
