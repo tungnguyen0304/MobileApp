1a33b1d2bc9c051203fa63d5c4f82d7ad6f5d4c9
Huy Ly <huylg2109@gmail.com>
2023-04-03 11:01:50 +0700

    Fix bug import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
 import { RunsModule } from './runs/runs.module';
 
 @Module({
