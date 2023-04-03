afc97fc05526847f733b4ee12b46ec49c2d00b39
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2023-04-03 14:59:41 +0700

    duc: add Swagger Docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Tạo đối tượng DocumentBuilder
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for my NestJS app')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  // Tạo tài liệu API từ DocumentBuilder
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Thiết lập các tùy chọn của Swagger UI
+  const options = {
+    explorer: true,
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+    },
+  };
+
+  // Đăng ký tài liệu API và Swagger UI Express middleware
+  SwaggerModule.setup('api', app, document, options);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
