220c186f537bc06cdbab83f5572397b6da0afb8b
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2023-04-17 10:18:35 +0700

    duc: scan

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -13,15 +13,56 @@ import {
 } from "react-native";
 import LottieView from "lottie-react-native";
 import { Animated } from "react-native";
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function MainScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [mssv, setMssv] = useState(0);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setMssv(data);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text >Main Screen</Text>
+      {scanned || <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />}
+      {scanned && <Text style={styles.discover}>Kết quả nhận được là: {mssv}</Text>}
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
+
 const styles = StyleSheet.create({
+  discover: {
+    fontWeight: "bold",
+    fontSize: 30,
+    width: 300,
+    height: 100,
+    textAlign: "center",
+    marginTop: 10,
+  },
   container: {
     flexGrow: 1,
     justifyContent: "center", // can giua theo truc y
@@ -30,3 +71,4 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
 });
+
